ceaf440406291c5f742a85c3b5e7f3959a685b16
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-11-03 16:01:38 -0300

    feat(feat/foods): add react-query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,12 @@
 import '../assets/styles/global.css'
 import { Slot } from 'expo-router'
 
+import { HttpClientProvider } from '@/presentation/shared/providers/http-client-provider'
+
 export default function RootLayout() {
-  return <Slot />
+  return (
+    <HttpClientProvider>
+      <Slot />
+    </HttpClientProvider>
+  )
 }
