db44b40e8d6b1d5e81c11b3a0834f4b56d21f574
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-11-03 01:32:53 -0300

    feat section component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,24 @@
 import Constants from 'expo-constants'
 import { ScrollView, View } from 'react-native'
 
-import { Banner, Header, Search } from '@/modules/core/presentation'
+import { Banner, Header, Search, Section } from '@/modules/core/presentation'
 
 const statusBarHeight = Constants.statusBarHeight
 
 export default function Home() {
   return (
-    <ScrollView className="flex-1 bg-slate-200 ">
+    <ScrollView className="flex-1 bg-slate-200">
       <View className="w-full gap-2 px-4 pt-2.5" style={{ marginTop: statusBarHeight }}>
         <Header />
         <Banner />
         <Search />
       </View>
+      <Section
+        title="Famosos no DevFood"
+        size="text-lg"
+        labelButton="Ver todos"
+        actionButton={() => console.log('Veja mais')}
+      />
     </ScrollView>
   )
 }
--- src/modules/core/presentation/index.ts
+++ src/modules/core/presentation/index.ts
@@ -1,3 +1,4 @@
 export * from './components/header'
 export * from './components/banner'
 export * from './components/search'
+export * from './components/section'
